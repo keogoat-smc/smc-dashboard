020f0fd2988d8c7b1ab37106a22069a3d0caf0b9
keora <keora@example.com>
2026-06-08 22:30:30 +0200

    fix flask backend

--- app.py
+++ app.py
@@ -8,7 +8,17 @@ def home():
 
 @app.route("/scan")
 def scan():
-    return jsonify({"status": "SMC dashboard working 🚀"})
+    return jsonify([
+        {
+            "symbol": "GOLD",
+            "price": 2350,
+            "type": "LONG",
+            "sl": 2330,
+            "tp": 2390,
+            "risk": 5.0,
+            "score": 75
+        }
+    ])
 
 if __name__ == "__main__":
     app.run(debug=True)
